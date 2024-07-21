70ea0e8832e440c08c18025f555332f771607d95
amaher <amaher112@su.edu>
2024-07-21 17:49:28 -0400

    added 2 decimal points and dollar sign

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const twentyBtn = document.getElementById("20");
 const total = document.getElementById("total");
 const tip = document.getElementById("tip");
 
-// function to calculate total
+
 function getTotalFifteen() {
   event.preventDefault();
   const billValue = bill.value;
@@ -13,32 +13,32 @@ function getTotalFifteen() {
   billWithFifteen = billTotal * 0.15;
   console.log(billWithFifteen);
 
-  tip.textContent = billWithFifteen;
+  tip.textContent = '$' + billWithFifteen.toFixed(2);
   const bill15Total = (billWithFifteen += billTotal);
   console.log(bill15Total);
 
-  total.textContent = bill15Total;
+  total.textContent = '$' + bill15Total.toFixed(2);
 }
 
 function getTotalEighteen() {
   event.preventDefault();
   const billValue = bill.value;
   const billTotal = Number.parseFloat(billValue);
-  billWithEighteen = billTotal*0.18;
+  billWithEighteen = billTotal * 0.18;
 
-  tip.textContent = billWithEighteen;
+  tip.textContent = '$' + billWithEighteen.toFixed(2);
   const bill18Total = (billWithEighteen += billTotal);
-  total.textContent = bill18Total
+  total.textContent = '$' + bill18Total.toFixed(2);
 }
 
 function getTotalTwenty() {
   event.preventDefault();
   const billValue = bill.value;
   const billTotal = Number.parseFloat(billValue);
-  billWithTwenty = billTotal*0.2;
-  tip.textContent = billWithTwenty;
+  billWithTwenty = billTotal * 0.2;
+  tip.textContent = '$' + billWithTwenty.toFixed(2);
   const bill20Total = (billWithTwenty += billTotal);
-  total.textContent = bill20Total
+  total.textContent = '$' + bill20Total.toFixed(2);
 }
 
 fifteenBtn.addEventListener("click", getTotalFifteen);
