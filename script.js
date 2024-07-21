1f576ac959fcf302fa0553bbc27c1c4ca84b5637
amaher <amaher112@su.edu>
2024-07-21 17:42:15 -0400

    added 18 functionality

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-const bill = document.getElementById('bill');
-const fifteenBtn = document.getElementById('15');
-const eighteenBtn = document.getElementById('18');
-const twentyBtn = document.getElementById('20');
-const total = document.getElementById('total');
-const tip = document.getElementById('tip');
-
+const bill = document.getElementById("bill");
+const fifteenBtn = document.getElementById("15");
+const eighteenBtn = document.getElementById("18");
+const twentyBtn = document.getElementById("20");
+const total = document.getElementById("total");
+const tip = document.getElementById("tip");
 
 // function to calculate total
 function getTotalFifteen() {
-    event.preventDefault();
-    const billValue = bill.value;
-    const billTotal = Number.parseFloat(billValue)
-    billWithFifteen = (billTotal*.15)
-    console.log(billWithFifteen)
-tip.textContent = billWithFifteen
-    const bill15Total = (billWithFifteen+=billTotal)
-    console.log(bill15Total)
-    total.textContent = bill15Total;
+  event.preventDefault();
+  const billValue = bill.value;
+  const billTotal = Number.parseFloat(billValue);
+  billWithFifteen = billTotal * 0.15;
+  console.log(billWithFifteen);
+
+  tip.textContent = billWithFifteen;
+  const bill15Total = (billWithFifteen += billTotal);
+  console.log(bill15Total);
+
+  total.textContent = bill15Total;
 }
 
 function getTotalEighteen() {
-    event.preventDefault();
+  event.preventDefault();
+  const billValue = bill.value;
+  const billTotal = Number.parseFloat(billValue);
+  billWithEighteen = billTotal*0.18;
 
+  tip.textContent = billWithEighteen;
+  const bill18Total = (billWithEighteen += billTotal);
+  total.textContent = bill18Total
 }
 
 function getTotalTwenty() {
-    event.preventDefault();
-
+  event.preventDefault();
 }
 
-fifteenBtn.addEventListener('click', getTotalFifteen)
-eighteenBtn.addEventListener('click', getTotalEighteen)
-twentyBtn.addEventListener('click', getTotalTwenty)
+fifteenBtn.addEventListener("click", getTotalFifteen);
+eighteenBtn.addEventListener("click", getTotalEighteen);
+twentyBtn.addEventListener("click", getTotalTwenty);
