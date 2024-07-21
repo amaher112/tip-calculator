19a246778b81e43923944b0ee99e33fa31c91bd9
amaher <amaher112@su.edu>
2024-07-21 17:37:15 -0400

    finished 15 button function

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ const fifteenBtn = document.getElementById('15');
 const eighteenBtn = document.getElementById('18');
 const twentyBtn = document.getElementById('20');
 const total = document.getElementById('total');
+const tip = document.getElementById('tip');
 
 
 // function to calculate total
 function getTotalFifteen() {
     event.preventDefault();
-    const billTotal = bill.value;
-    console.log(billTotal)
-    billWithFifteen = (billTotal*5)
+    const billValue = bill.value;
+    const billTotal = Number.parseFloat(billValue)
+    billWithFifteen = (billTotal*.15)
     console.log(billWithFifteen)
+tip.textContent = billWithFifteen
+    const bill15Total = (billWithFifteen+=billTotal)
+    console.log(bill15Total)
+    total.textContent = bill15Total;
 }
 
 function getTotalEighteen() {
